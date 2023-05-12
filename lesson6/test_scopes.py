07067552013d08674097241ec4c54b91b786142b
Mila Podchasova <Pod4asova20@gmail.com>
2023-05-13 01:10:18 +0300

    Update test_scopes.py

--- lesson6/test_scopes.py
+++ lesson6/test_scopes.py
@@ -1,5 +1,7 @@
 import pytest
-
+import time
+import requests
+import sqlite3
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
